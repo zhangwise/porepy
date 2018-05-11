68423d128d05449371eebe6240a545fa6d10ae97
Alessio Fumagalli <alessio.fumagalli1984@gmail.com>
2018-05-11 09:10:21 +0200

    new class mass vem primal

--- src/porepy/numerics/vem/primal_mass.py
+++ src/porepy/numerics/vem/primal_mass.py
@@ -21,7 +21,7 @@ from porepy.utils import comp_geom as cg
 
 #------------------------------------------------------------------------------#
 
-#class PrimalVEMMixedDim(SolverMixedDim):
+#class PrimalMassVEMMixedDim(SolverMixedDim):
 #
 #    def __init__(self, physics='flow'):
 #        self.physics = physics
@@ -34,7 +34,7 @@ from porepy.utils import comp_geom as cg
 
 #------------------------------------------------------------------------------#
 
-class PrimalVEM(Solver):
+class PrimalMassVEM(Solver):
 
 #------------------------------------------------------------------------------#
 
@@ -85,24 +85,16 @@ class PrimalVEM(Solver):
         rhs: array (g.num_faces+g_num_cells)
             Right-hand side which contains the boundary conditions.
         """
-        M, bc_weight = self.matrix(g, data, bc_weight=True)
-        return M, self.rhs(g, data, bc_weight)
+        M = self.matrix(g, data)
+        return M, self.rhs(g, data)
 
 #------------------------------------------------------------------------------#
 
-    def matrix(self, g, data, bc_weight=False):
+    def matrix(self, g, data):
         """
         Return the matrix for a discretization of a second order elliptic equation
         using primal virtual element method. See self.matrix_rhs for a detaild
         description.
-
-        Additional parameter:
-        --------------------
-        bc_weight: to compute the infinity norm of the matrix and use it as a
-            weight to impose the boundary conditions. Default True.
-
-        Additional return:
-        weight: if bc_weight is True return the weight computed.
 
         """
         # Allow short variable names in backend function
@@ -110,16 +102,12 @@ class PrimalVEM(Solver):
 
         # If a 0-d grid is given then we return an identity matrix
         if g.dim == 0:
-            M = sps.csr_matrix((self.ndof(g), self.ndof(g)))
-            if bc_weight:
-                return M, 1
-            return M
+            return sps.csr_matrix((self.ndof(g), self.ndof(g)))
 
         # Retrieve the permeability, boundary conditions, and aperture
         # The aperture is needed in the hybrid-dimensional case, otherwise is
         # assumed unitary
         param = data['param']
-        k = param.get_tensor(self)
         bc = param.get_bc(self)
         a = param.get_aperture()
 
@@ -136,15 +124,6 @@ class PrimalVEM(Solver):
         # surface coordinates in 1d and 2d)
         c_centers, f_normals, f_centers, R, dim, node_coords = cg.map_grid(g)
 
-        if not data.get('is_tangential', False):
-                # Rotate the permeability tensor and delete last dimension
-                if g.dim < 3:
-                    k = k.copy()
-                    k.rotate(R)
-                    remove_dim = np.where(np.logical_not(dim))[0]
-                    k.perm = np.delete(k.perm, (remove_dim), axis=0)
-                    k.perm = np.delete(k.perm, (remove_dim), axis=1)
-
         # In the virtual cell approach the cell diameters should involve the
         # apertures, however to keep consistency with the hybrid-dimensional
         # approach and with the related hypotheses we avoid.
@@ -157,6 +136,8 @@ class PrimalVEM(Solver):
         J = np.empty(size, dtype=int)
         dataIJ = np.empty(size)
         idx = 0
+
+        ws, xi = cg.simplex_reference_quadrature(g.dim)
 
         for c in np.arange(g.num_cells):
             # For the current cell retrieve its faces
@@ -176,12 +157,12 @@ class PrimalVEM(Solver):
                 nodes_l = np.array([nodes_map[n] for n in nodes_fn[loc]])
                 node_faces[nodes_l, f_loc] = True
 
-            # Compute the siff-H1 local matrix
-            A = self.stiffH1(a[c]*k.perm[0:g.dim, 0:g.dim, c], c_centers[:, c],
-                             g.cell_volumes[c], normals, diams[c],
-                             node_coords[:, nodes_loc], node_faces)
+            # Compute the mass-H1 local matrix
+            A = self.massH1(a[c], c_centers[:, c],
+                            g.cell_volumes[c], normals, diams[c],
+                            node_coords[:, nodes_loc], node_faces, ws, xi)
 
-            # Save values for Hdiv-mass local matrix in the global structure
+            # Save values for H1-mass local matrix in the global structure
             cols = np.tile(nodes_loc, (nodes_loc.size, 1))
             loc_idx = slice(idx, idx+cols.size)
             I[loc_idx] = cols.T.ravel()
@@ -190,67 +171,32 @@ class PrimalVEM(Solver):
             idx += cols.size
 
         # Construct the global matrices
-        M = sps.csr_matrix((dataIJ, (I, J)))
-
-        norm = sps.linalg.norm(M, np.inf) if bc_weight else 1
-
-        # assign the Dirichlet boundary conditions
-        if bc and np.any(bc.is_dir):
-            is_dir = np.where(bc.is_dir)[0]
-            M[is_dir, :] *= 0
-            M[is_dir, is_dir] = norm
-
-        if bc_weight:
-            return M, norm
-        return M
+        return sps.csr_matrix((dataIJ, (I, J)))
 
 #------------------------------------------------------------------------------#
 
-    def rhs(self, g, data, bc_weight=1):
+    def rhs(self, g, data):
         """
         Return the righ-hand side for a discretization of a second order elliptic
         equation using dual virtual element method. See self.matrix_rhs for a detaild
         description.
 
-        Additional parameter:
-        --------------------
-        bc_weight: to use the infinity norm of the matrix to impose the
-            boundary conditions. Default 1.
-
         """
         # Allow short variable names in backend function
         # pylint: disable=invalid-name
 
-        param = data['param']
-        f = param.get_source(self)
-
-        if g.dim == 0:
-            return np.array(f)
-
-        bc = param.get_bc(self)
-        bc_val = param.get_bc_val(self)
-
-        assert not bool(bc is None) != bool(bc_val is None)
-
-        rhs = np.zeros(self.ndof(g))
-        if bc is None:
-            return rhs
-
-        if np.any(bc.is_dir):
-            is_dir = np.where(bc.is_dir)[0]
-            rhs[is_dir] = bc_weight*bc_val[is_dir]
-
-        return rhs
+        return np.zeros(self.ndof(g))
 
 #------------------------------------------------------------------------------#
 
-    def stiffH1(self, K, c_center, c_volume, normals, diam, coord, node_faces):
-        """ Compute the local stiffness H1 matrix using the primal vem approach.
+    def massH1(self, apert, c_center, c_volume, normals, diam, coord,
+               node_faces, ws, xi):
+        """ Compute the local mass Hdiv matrix using the mixed vem approach.
 
         Parameters
         ----------
-        K : ndarray (g.dim, g.dim)
-            Permeability of the cell.
+        apert : ndarray (g.dim, g.dim)
+            Aperture of the cell.
         c_center : array (g.dim)
             Cell center.
         c_volume : scalar
@@ -269,7 +215,6 @@ class PrimalVEM(Solver):
         """
         # Allow short variable names in this function
         # pylint: disable=invalid-name
-
         dim = coord.shape[0]
         num_nodes = coord.shape[1]
         num_mono = dim+1
@@ -302,11 +247,22 @@ class PrimalVEM(Solver):
         assert np.allclose(G, np.dot(F, D)),\
                                          "G\n"+str(G)+"\nF*D\n"+str(np.dot(F,D))
 
+        # local matrix H
+        H = np.zeros((num_mono, num_mono))
+        H[0, 0] = c_volume
+
+        pts, weights = cg.quadrature_rule(c_center, coord, node_faces, ws, xi)
+        for i in np.arange(1, num_mono):
+            for j in np.arange(1, num_mono):
+                for pt, weight in zip(pts.T, weights):
+                    H[i, j] += weight*mono[i](pt)*mono[j](pt)
+
         # local matrix Pi_s
         Pi_s = np.linalg.solve(G, F)
         I_Pi = np.eye(num_nodes) - np.dot(D, Pi_s)
 
-        # local H1-stiff matrix
-        return np.dot(Pi_s.T, np.dot(Gtilde, Pi_s)) + np.dot(I_Pi.T, I_Pi)
+        # local H1-mass matrix
+        M = np.dot(Pi_s.T, np.dot(H, Pi_s)) + c_volume*np.dot(I_Pi.T, I_Pi)
+        return apert*M
 
 #------------------------------------------------------------------------------#
